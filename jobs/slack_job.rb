501041c3b9de795de15beffd3d0f631976150627
Christian Nash <christiann@slalom.com>
2016-04-19 15:40:34 -0400

    bad print

--- jobs/slack_job.rb
+++ jobs/slack_job.rb
@@ -16,8 +16,6 @@ SCHEDULER.every '5s', :first_in => 0 do |job|
 	resp = Net::HTTP.get_response(URI.parse(url))
 	obj = JSON.parse(resp.body)
 
-	pp(obj["messages"])
-
 	# Grab first 8 messages
 	if obj["messages"]
 		messages = obj["messages"][0..7]
